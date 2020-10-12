db185d7f9ec184446719c976791c4821672ce627
蒲宇 <1137863365@qq.com>
2020-10-12 17:15:27 +0800

    leetCode.js

--- src/算法相关/postorder.js
+++ src/算法相关/postorder.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Martin
  * @Date: 2020-10-11 10:56:33
- * @LastEditTime: 2020-10-11 10:57:01
+ * @LastEditTime: 2020-10-11 16:55:14
  * @FilePath: \Daily_question\src\算法相关\postorder.js
  */
 /**
@@ -12,4 +12,25 @@ const postorder = (root) => {
     postorder(root.left)
     postorder(root.right)
     console.log(root.val)
+}
+
+/**
+ * 迭代实现
+ * 通过之前用堆栈迭代实现前序遍历，将结果倒置，并将做变为右
+ */
+
+const postorder = (root) => {
+
+    const outputStack = []
+    const stack = [root]
+    while (stack.length) {
+        const n = stack.pop();
+        outputStack.push(n)
+        if(n.left) stack.push(n.left)
+        if(n.right) stack.push(n.right)
+    }
+    while(outputStack.length){
+        const n = outputStack.pop()
+        console.log(n)
+    }
 }
