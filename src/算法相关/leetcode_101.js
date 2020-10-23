048f12d781ffcea8d8ef23e3913dfde64f047aa8
蒲宇 <1137863365@qq.com>
2020-10-23 22:29:12 +0800

    leetcode

--- src/算法相关/leetcode_101.js
+++ src/算法相关/leetcode_101.js
@@ -1,13 +1,15 @@
 /*
  * @Author: Martin
  * @Date: 2020-10-13 19:58:59
- * @LastEditTime: 2020-10-13 20:33:02
+ * @LastEditTime: 2020-10-23 22:28:52
  * @FilePath: \Daily_question\src\算法相关\leetcode_101.js
  */
 /**
  * 对称二叉树。考虑使用递归。因为为镜像对称，所以需要两个根节点的值相等。
- * 且右和左子节点的值分别等于另一个节点的左和右子节点。步骤：先比较根节点的left和right，如果不等则直接返回。
- * 如果相等就比较left的左节点和right的右节点。再比较left的右节点和right的左节点，继续递归比较下面的节点
+ * 且右和左子节点的值分别等于另一个节点的左和右子节点。
+ * 步骤：先比较根节点的left和right，如果不等则直接返回。
+ * 如果相等就比较left的左节点和right的右节点。
+ * 再比较left的右节点和right的左节点，继续递归比较下面的节点
  */
 var isSymmetric = function (root) {
     if (root === null) { return true }
